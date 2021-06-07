9c7dc0f51ca0a56935b6babdbb57d61978ddda57
Isaque Schuwarte <isaque_schuwarte@hotmail.com>
2021-06-07 15:48:32 -0300

    Created Interaction with Database

--- ProjetoLogin/DAL/LoginDALCommands.cs
+++ ProjetoLogin/DAL/LoginDALCommands.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,32 @@ namespace ProjetoLogin.DAL
 {
     class LoginDALCommands
     {
-        public bool has;
-        public String message;
+        public bool has = false;
+        public String message = "";
+        SqlCommand cmd = new SqlCommand();
+        Connection con = new Connection();
+        SqlDataReader dr;
 
         // Verfica se tem no banco SQL
         public bool CheckLogin(String login, String password)
         {
+            cmd.CommandText = "select * from nameOfList where email = @login and password = @password";
+            cmd.Parameters.AddWithValue("@login", login);
+            cmd.Parameters.AddWithValue("@password", password);
+
+            try
+            {
+                cmd.Connection = con.Connect();
+                dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    has = true;
+                }
+            }
+            catch (SqlException)
+            {
+                this.message = "Erro com o banco de dados!";
+            }
             return has;
         }
 
